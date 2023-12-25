d4a022e6822b31e363489aed7dc62a7e44c8bae7
inotia00 <108592928+inotia00@users.noreply.github.com>
2023-12-25 18:06:01 +0900

    fix: unintentional click behavior in `ripLibs` settings

--- lib/ui/widgets/settingsView/settings_riplibs.dart
+++ lib/ui/widgets/settingsView/settings_riplibs.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/widgets/I18nText.dart';
-import 'package:revanced_manager/ui/views/patcher/patcher_viewmodel.dart';
-import 'package:revanced_manager/ui/views/patches_selector/patches_selector_viewmodel.dart';
 import 'package:revanced_manager/ui/views/settings/settings_viewmodel.dart';
 
 class SRipLibs extends StatefulWidget {
@@ -12,8 +10,6 @@ class SRipLibs extends StatefulWidget {
 }
 
 final _settingsViewModel = SettingsViewModel();
-final _patchesSelectorViewModel = PatchesSelectorViewModel();
-final _patcherViewModel = PatcherViewModel();
 
 class _SRipLibsState extends State<SRipLibs> {
   @override
@@ -36,12 +32,6 @@ class _SRipLibsState extends State<SRipLibs> {
         setState(() {
           _settingsViewModel.showRipLibs(value);
         });
-        if (!value) {
-          _patcherViewModel.selectedPatches
-              .removeWhere((patch) => patch.compatiblePackages.isEmpty);
-          _patchesSelectorViewModel.selectedPatches
-              .removeWhere((patch) => patch.compatiblePackages.isEmpty);
-        }
       },
     );
   }

3217f9ecba026f579b39d66048da9702fa1d1a38
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-06-23 16:09:50 +0900

    fix: apply code review suggestions

--- lib/ui/widgets/patchesSelectorView/patch_options_fields.dart
+++ lib/ui/widgets/patchesSelectorView/patch_options_fields.dart
@@ -1,12 +1,13 @@
 import 'dart:io';
 
-import 'package:device_info_plus/device_info_plus.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:revanced_manager/app/app.locator.dart';
 import 'package:revanced_manager/gen/strings.g.dart';
 import 'package:revanced_manager/models/patch.dart';
+import 'package:revanced_manager/services/manager_api.dart';
 import 'package:revanced_manager/ui/widgets/shared/custom_card.dart';
 
 class BooleanPatchOption extends StatelessWidget {
@@ -386,6 +387,8 @@ class TextFieldForPatchOption extends StatefulWidget {
 }
 
 class _TextFieldForPatchOptionState extends State<TextFieldForPatchOption> {
+  final ManagerAPI _managerAPI = locator<ManagerAPI>();
+
   final TextEditingController controller = TextEditingController();
   String? selectedKey;
   String? defaultValue;
@@ -508,15 +511,10 @@ class _TextFieldForPatchOptionState extends State<TextFieldForPatchOption> {
                   // manageExternalStorage permission is required for file/folder selection
                   // otherwise, the app will not complain, but the patches will error out
                   // the same way as if the user selected an empty file/folder.
-                  // Android 10 and above requires the manageExternalStorage permission
+                  // Android 11 and above requires the manageExternalStorage permission
                   final Map<String, dynamic> availableActions = {
                     t.patchOptionsView.selectFilePath: () async {
-                      final androidVersion =
-                          await DeviceInfoPlugin().androidInfo.then((info) {
-                        return info.version.release;
-                      });
-                      if (Platform.isAndroid &&
-                          int.parse(androidVersion) >= 10) {
+                      if (_managerAPI.isScopedStorageAvailable) {
                         final permission =
                             await Permission.manageExternalStorage.request();
                         if (!permission.isGranted) {
@@ -532,12 +530,7 @@ class _TextFieldForPatchOptionState extends State<TextFieldForPatchOption> {
                       widget.onChanged(controller.text);
                     },
                     t.patchOptionsView.selectFolder: () async {
-                      final androidVersion =
-                          await DeviceInfoPlugin().androidInfo.then((info) {
-                        return info.version.release;
-                      });
-                      if (Platform.isAndroid &&
-                          int.parse(androidVersion) >= 10) {
+                      if (_managerAPI.isScopedStorageAvailable) {
                         final permission =
                             await Permission.manageExternalStorage.request();
                         if (!permission.isGranted) {

fd3d3d463e605ca6bb95e78ec79e873055635e3c
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-09-07 00:52:16 +0900

    fix: resolve EACCESS error in special cases

--- android/app/src/main/kotlin/app/revanced/manager/flutter/MainActivity.kt
+++ android/app/src/main/kotlin/app/revanced/manager/flutter/MainActivity.kt
@@ -223,6 +223,8 @@ class MainActivity : FlutterActivity() {
         ripArchitectureList: List<String>
     ) {
         val inFile = File(inFilePath)
+        inFile.setWritable(true)
+        inFile.setReadable(true)
         val outFile = File(outFilePath)
         val integrations = File(integrationsPath)
         val keyStoreFile = File(keyStoreFilePath)

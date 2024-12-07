d3e435fd88c38a3c888b9e64ab16c3c5a7c38a15
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-12-07 14:59:48 +0900

    fix: revert commit https://github.com/ReVanced/revanced-manager/commit/5c68d513a38dcca1b884429c28f5bb32697bf37d to fix textfield cursor issue

--- lib/ui/views/patches_selector/patches_selector_viewmodel.dart
+++ lib/ui/views/patches_selector/patches_selector_viewmodel.dart
@@ -57,6 +57,7 @@ class PatchesSelectorViewModel extends BaseViewModel {
   void navigateToPatchOptions(List<Option> setOptions, Patch patch) {
     _managerAPI.options = setOptions;
     _managerAPI.selectedPatch = patch;
+    _managerAPI.modifiedOptions.clear();
     _navigationService.navigateToPatchOptionsView();
   }
 
